07fb51142cc2612e50efc5507ee5ecfa3ebd0cd2
takazawa1204 <ckicken1204@gmail.com>
2021-10-18 08:21:57 +0000

    非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,20 @@
 class FavoritesController < ApplicationController
+   before_action :authenticate_user!
+  
   def create
-    itenerary = Itenerary.find(params[:itenerary_id])
-    favorite = current_user.favorites.new(itenerary_id: itenerary.id)
+    @itenerary = Itenerary.find(params[:itenerary_id])
+    favorite = current_user.favorites.new(itenerary_id: @itenerary.id)
     favorite.save
-    redirect_to iteneraries_path
+    # redirect_to iteneraries_path
   end
-  
-  
+
+
   def destroy
-    itenerary = Itenerary.find(params[:itenerary_id])
-    favorite = current_user.favorites.find_by(itenerary_id: itenerary.id)
+    @itenerary = Itenerary.find(params[:itenerary_id])
+    favorite = current_user.favorites.find_by(itenerary_id: @itenerary.id)
     favorite.destroy
-    redirect_to iteneraries_path
+    # redirect_to iteneraries_path
   end
-  
-  
+
+
 end
